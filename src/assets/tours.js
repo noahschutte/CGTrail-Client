2d2e93f625f7f109a55e530eceb989f4616c4033
Jameson Welling <jameson@spiredigital.com>
2018-08-05 13:11:34 -0600

    initial commit

---
+++ src/assets/tours.js
@@ -0,0 +1,16 @@
+const tours = [
+  {
+    name: 'Lazy Sunday',
+    imgUrl: 'http://www.gowaddle.com/wp-content/uploads/2017/02/paris-cafe-croissant-coffee-and-newspaper.png'
+  },
+  {name: 'Beer and Wine',
+    imgUrl: 'http://hooversun.com/downloads/11379/download/beer%20and%20wine.jpg?cb=03446c2bc39c6829a36f99b877efe229&w=480'},
+  {name: 'Fine Dining',
+    imgUrl: 'https://wvtourism.com/wp-content/uploads/2017/10/dining-e1508956552374.jpg'},
+  {name: 'Caffeine Fix',
+    imgUrl: 'https://c1.staticflickr.com/5/4333/37214271682_37e136e1cb_b.jpg'},
+  {name: 'A Taste of __________',
+    imgUrl: 'http://bodyambition.com/wp-content/uploads/2015/02/anti-inflam.jpg'}
+]
+
+module.exports = tours
